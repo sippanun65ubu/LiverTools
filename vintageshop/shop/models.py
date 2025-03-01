995be5b437c2e91ab34c7da61b5c284bcf3cfd04
Thanawit3009 <126285358+Thanawit3009@users.noreply.github.com>
2025-03-02 02:47:58 +0700

    add chat userandadmin

--- vintageshop/shop/models.py
+++ vintageshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -28,3 +29,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_messages")
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="received_messages")
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver} at {self.timestamp}"
--- vintageshop/shop/urls.py
+++ vintageshop/shop/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),  # หน้าแสดงสินค้า
+    path('', views.product_list, name='product_list'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('delete-product/<int:product_id>/', views.delete_product, name='delete_product'),
     path('edit-product/<int:product_id>/', views.edit_product, name='edit_product'), 
+    path('chat/', views.user_chat, name='user_chat'),
+    path('admin-chat/', views.admin_chat, name='admin_chat'),  # ✅ แก้ path เป็น 'admin-chat/'
 ]
